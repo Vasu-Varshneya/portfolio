e846f1205596dceb650c3f992ab66a1d0f73b8b3
Vasu <148642918+Vasu-Varshneya@users.noreply.github.com>
2025-08-13 18:56:07 +0530

    Update page.tsx12

--- app/page.tsx
+++ app/page.tsx
@@ -103,7 +103,7 @@ export default function Component() {
           <div className="container px-4 md:px-6">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-8">Projects</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-              {/* <Card>
+              <Card>
                 <CardHeader>
                   <CardTitle>Project 1</CardTitle>
                   <CardDescription>• Built an interactive job portal for employers and job seekers.
@@ -116,7 +116,7 @@ export default function Component() {
                     <Button>View Project</Button>
                   </Link>
                 </CardContent>
-              </Card> */}
+              </Card>
               <Card>
                 <CardHeader>
                   <CardTitle>Project 2</CardTitle>
